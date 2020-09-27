737d17b495f4a8a776ba553142f268d2aeb21274
Sam959595 <sambertovv@gmail.com>
2020-09-27 15:27:43 +0500

    first commit

---
+++ recent-event/ru/js/lazy.js
@@ -0,0 +1,14 @@
+let u = [
+    '/uploads/hero_about.jpg',
+    '/uploads/hero_history.jpg',
+    '/uploads/hero_contact.jpg',
+    '/uploads/hero_services.jpg'
+];
+
+window.onload = function() {
+    let i = $('.lazy');
+
+    i.css({
+        opacity: '1'
+    });
+};
