2f746535429ee954321d4ea5808c333d1f293f01
Sam959595 <sambertovv@gmail.com>
2020-11-06 15:15:10 +0500

    first commit

--- js/search.js
+++ js/search.js
@@ -1,1 +1,7 @@
 globalThis == this //для различных редакторов, сгруппировать глобальное обращение
+
+let i = document.getElementById('s');
+
+i.addEventListener('click', function(){
+    console.log('click');
+})
