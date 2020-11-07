be540b4ec72168e811b042725495c532f6959e99
Sam959595 <sambertovv@gmail.com>
2020-11-07 13:32:20 +0500

    first commit

--- js/search.js
+++ js/search.js
@@ -5,3 +5,10 @@ var i = document.getElementById('s');
 i.addEventListener('click', function(){
     console.log('click');
 })
+
+let e = $("body");
+e.addClass("color", function(){
+    e.css({
+        'background-color':'#435'
+    });
+});
