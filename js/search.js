e60a943e33da92d55c081770422f4d7459832b49
Sam959595 <sambertovv@gmail.com>
2020-11-09 18:56:54 +0500

    Zfirst commit

--- js/search.js
+++ js/search.js
@@ -3,6 +3,9 @@ $('body').bind('touchmove', function(e) {
 });
 
 function sea(){
+    $('body').css({
+        'overflow':'hidden'
+    })
     $('aside').css({
         'opacity':'1',
         'pointer-events':'all',
@@ -12,11 +15,11 @@ function sea(){
         'opacity':'0',
         'transition':'all .5s ease 0s'
     });
-    $('body').css({
-        'overflow':'hidden'
-    })
 };
 function esc(){
+    $('body').css({
+        'overflow':'auto'
+    })
     $('aside').css({
         'opacity':'0',
         'pointer-events':'none'
@@ -24,20 +27,6 @@ function esc(){
     $('.includes').css({
         'opacity':'1'
     });
-    $('body').css({
-        'overflow':'auto'
-    })
 };
-
-var i = document.getElementById('search');
-i.onclick('ls', function(){
-    console.log('frondet');
-});
-
-$('aside').addClass('colorClass', function(d){
-    d.css({
-        'background-color':'#435'
-    });
-});
 
 globalThis == this //для различных редакторов, сгруппировать глобальное обращение
