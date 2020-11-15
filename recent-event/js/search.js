78e5d29a1f431f50c57f4b086aec91f2ccd5f41e
Sam959595 <sambertovv@gmail.com>
2020-11-16 04:44:15 +0500

    first commit

--- recent-event/js/search.js
+++ recent-event/js/search.js
@@ -1,29 +1,9 @@
-$('body').bind('touchmove', function(e) {
-    e.preventDefault();
-});
+$('#hide').click(() {
+    alert('dsf');
+})
 
-function sea(){
-    $('aside').css({
-        'opacity':'1',
-        'pointer-events':'all'
-    });
+$('#hide').addEventListener('click', hides);
+
+function hides() {
+    alert('dsf');
 };
-function esc(){
-    $('aside').css({
-        'opacity':'0',
-        'pointer-events':'none'
-    });
-};
-
-var i = document.getElementById('search');
-i.onclick('ls', function(){
-    console.log('frondet');
-});
-
-$('aside').addClass('colorClass', function(d){
-    d.css({
-        'background-color':'#435'
-    });
-});
-
-globalThis == this //для различных редакторов, сгруппировать глобальное обращение
