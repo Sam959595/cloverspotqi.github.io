0408ff4a22bfb1feaaec61485b9057cf1f8aa0a0
Sam959595 <sambertovv@gmail.com>
2020-11-22 09:57:26 +0500

    fisrt commit

--- recent-event/shoe/js/script.js
+++ recent-event/shoe/js/script.js
@@ -1,7 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-
-    $('.hide button').click(function() {
-        $(this).remove();
-        $('dl .hide')[0].classList.remove('close');
-    });
-});
+window.onload = function() {
+    $('blockquote p')[0].classList.add('quote')
+}
