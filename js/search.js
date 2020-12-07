ebd8e95af4f25c7fd2313e18c60897a7e801a4f0
Sam959595 <sambertovv@gmail.com>
2020-12-07 22:17:58 +0500

    fisrt commit

--- js/search.js
+++ js/search.js
@@ -1,27 +1,24 @@
 document.addEventListener('DOMContentLoaded', function() {
 
     $('nav span').click(function() {
-        $('body')[0].classList.add('search');
 
-        $('aside')[0].classList.add('visible');
+        $('body')[0].classList.add('search');
+        $('aside, section.includes')[0].classList.add('visible');
     });
     $('aside main:first-child div:first-child button').click(function() {
-        $('body')[0].classList.remove('search');
 
-        $('aside')[0].classList.remove('visible');
+        $('body')[0].classList.remove('search');
+        $('aside, section.includes')[0].classList.remove('visible');
     });
 
     $('aside form input').click(function() {
-        $('aside main:first-child div:last-child button')[0].classList.add('hide');
 
+        $('aside main:first-child div:last-child button')[0].classList.add('hide');
         $('aside main:first-child div:first-child')[0].classList.add('disappear');
     });
-
     $('aside main:first-child div:last-child button').click(function() {
-        $('body')[0].classList.remove('search');
-
+        
         $('aside main:first-child div:last-child button')[0].classList.remove('hide');
-
         $('aside main:first-child div:first-child')[0].classList.remove('disappear');
     });
 });
