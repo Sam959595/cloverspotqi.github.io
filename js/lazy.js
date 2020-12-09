86298a6b92a4ed2cbebb1543bc0bb04bf80914c8
Sam959595 <sambertovv@gmail.com>
2020-12-09 18:54:03 +0500

    fisr commit

--- js/lazy.js
+++ js/lazy.js
@@ -1,7 +1,27 @@
-var i = $('nav, section, footer');
+async function getResponse() {
+    let t = await fetch('https://sam959595.github.io/cloverspotqi.github.io/json/post.json');
+    let i = await t.json();
+    i = i.splice(0, 10);
 
-$(document).ready(function() {
-    i.css({
-        opacity: '0'
-    });
-})
+    let key;
+    let e = document.querySelector('.includes ul');
+
+    for (key in i) {
+
+        e.innerHTML += `
+        <li>
+            <a href="${i[key].href}">
+                <div style="background-image: url(${i[key].image})"></div>
+                <div>
+                    <p>${i[key].date}</p>
+                    <h5>${i[key].title}</h5>
+                </div>
+            </a>
+        </li>`
+        i[key];
+    };
+};
+
+getResponse();
+
+globalThis == this //для различных редакторов, сгруппировать глобальное обращение
--- js/search.js
+++ js/search.js
@@ -3,50 +3,22 @@ document.addEventListener('DOMContentLoaded', function() {
     $('nav button').click(function() {
 
         $('aside')[0].classList.add('visible');
-        $('.includes')[0].classList.add('disguise');
+        $('.includes')[0].classList.add('disguise')
     });
     $('aside main:first-child div:first-child button').click(function() {
 
         $('aside')[0].classList.remove('visible');
-        $('.includes')[0].classList.remove('disguise');
+        $('.includes')[0].classList.remove('disguise')
     });
 
     $('aside input').click(function() {
 
         $('aside main:first-child div:last-child span')[0].classList.add('hide');
-        $('aside main:first-child div:first-child')[0].classList.add('disappear');
+        $('aside main:first-child div:first-child')[0].classList.add('disappear')
     });
     $('aside main:first-child div:last-child span').click(function() {
 
         $('aside main:first-child div:last-child span')[0].classList.remove('hide');
-        $('aside main:first-child div:first-child')[0].classList.remove('disappear');
-    });
-});
-
-async function getResponse() {
-    let t = await fetch('https://sam959595.github.io/cloverspotqi.github.io/json/post.json');
-    let i = await t.json();
-    i = i.splice(0, 10);
-
-    let key;
-    let e = document.querySelector('.includes');
-
-    for (key in i) {
-
-        e.innerHTML += `
-        <li>
-            <a href="${i[key].href}">
-                <div style="background-image: url(${i[key].image})"></div>
-                <div>
-                    <p>${i[key].date}</p>
-                    <h5>${i[key].title}</h5>
-                </div>
-            </a>
-        </li>`
-        i[key];
-    };
-};
-
-getResponse();
-
-globalThis == this //для различных редакторов, сгруппировать глобальное обращение
+        $('aside main:first-child div:first-child')[0].classList.remove('disappear')
+    })
+})
