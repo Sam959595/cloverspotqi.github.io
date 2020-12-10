2381fbf93ecf7e0ad38fdb4a288850c903ddcef1
Sam959595 <sambertovv@gmail.com>
2020-12-10 18:03:35 +0500

    fisrt commit

--- js/search.js
+++ js/search.js
@@ -1,32 +1,33 @@
+
+
 document.addEventListener('DOMContentLoaded', function() {
 
     $('nav button').click(function() {
 
         $('aside')[0].classList.add('visible');
-        $('.includes')[0].classList.add('disguise')
+        $('.includes')[0].classList.add('disguise');
     });
     $('aside main:first-child div:first-child button').click(function() {
 
         $('aside')[0].classList.remove('visible');
-        $('.includes')[0].classList.remove('disguise')
+        $('.includes')[0].classList.remove('disguise');
     });
 
     $('aside input').click(function() {
 
         $('aside main:first-child div:last-child span')[0].classList.add('hide');
-        $('aside main:first-child div:first-child')[0].classList.add('disappear')
+        $('aside main:first-child div:first-child')[0].classList.add('disappear');
     });
     $('aside main:first-child div:last-child span').click(function() {
 
         $('aside main:first-child div:last-child span')[0].classList.remove('hide');
-        $('aside main:first-child div:first-child')[0].classList.remove('disappear')
-    })
-};
+        $('aside main:first-child div:first-child')[0].classList.remove('disappear');
+    });
+});
 
 async function getResponse() {
     let t = await fetch('https://sam959595.github.io/cloverspotqi.github.io/json/nav.json');
     let i = await t.json();
-    i = i.splice(0, 10);
 
     let r = null;
     let e = document.querySelector('.includes ul');
