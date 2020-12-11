d1f910080dd59f9820b2e676f472e4fb89dc42c4
Sam959595 <sambertovv@gmail.com>
2020-12-11 14:40:47 +0500

    fisrt commit

--- recent-event/js/search.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-
-    $('.hide button').click(function() {
-        $(this).remove();
-        $('dl .hide')[0].classList.remove('close');
-    });
-});
