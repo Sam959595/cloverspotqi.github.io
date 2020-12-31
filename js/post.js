9562d9630192d3dba389a0c31de36deecd23c5e0
Sam959595 <sambertovv@gmail.com>
2020-12-31 08:53:39 +0500

    fisrt commit

--- js/post.js
+++ js/post.js
@@ -1,25 +1,21 @@
-$(window).scroll(function() {
-    let er = 2;
+async function sech() {
+    let t = await fetch('json/post.json'),
+    b = await t.json(),
+    e = document.querySelector('.includes ul');
+    
+    b = b.splice(0, 5);
 
-    if ($(window).scrollTop() + $(window).height() >= $(document).height()) {
-        $.ajax({
-            url: 'json/post.json',
-            method: 'post',
-            data: ('er' : er),
-            beforeSend: function() {
+    for (let i = 0; i < b.length; i++) {
+        e.innerHTML += `<li><a href="${b[i].href}"><div style="background-image: url(${b[i].image})"></div><div><p>${b[i].date}</p><h5>${b[i].title}</h5></div></a></li>`;
+    }
 
-            }.done(function(data) {
-                data = jQuery.parseJSON(data);
-            });
-
-            if (data.length > 0) {
-                $.each(data, function(index, data) {
-
-                });
-            };
-        });
-    };
-});
+    $(window).scroll(function() {
+        if ($(window).scrollTop() + $(window).height() >= $(document).height()) {
+            b = b.splice(0, 1);
+        };
+    });
+};
+sech();
 
 //
 
