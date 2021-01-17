cf012c29336db7468af2512021757d39f64dbc5d
Sam959595 <sambertovv@gmail.com>
2021-01-17 17:17:27 +0500

    fisrt commit

--- js/index/post.js
+++ js/index/post.js
@@ -26,3 +26,21 @@ window.onload = function() {
 //
 
 console.log([1, 2, 5, 6, 8].filter(x => !(x % 2)).reduceRight((c, x) => c + Math.sqrt(x), 0));
+
+/* function funcs(str) {
+    document.write(str);
+}
+
+function riri(a, b) {
+    return a*b/3;
+}*/
+
+//funcs("kkk = " + riri(3, 5));
+
+
+function ds() {
+    let d = new Date();
+
+    document.write(d.getFullYear() + "-" + d.getMonth() + "-" + d.getDate())
+}
+ds()
