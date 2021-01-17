51a020dbbccf302e1d4f1d305069915cd4a12584
Sam959595 <sambertovv@gmail.com>
2021-01-17 18:21:20 +0500

    fisrt commit

--- js/index/post.js
+++ js/index/post.js
@@ -20,27 +20,8 @@ sech();
 //
 
 window.onload = function() {
-    $('blockquote p')[0].classList.add('quote');
+    let a = $('blockquote p'),
+    b = 'quote';
+
+    a[0].classList.add(b);
 };
-
-//
-
-console.log([1, 2, 5, 6, 8].filter(x => !(x % 2)).reduceRight((c, x) => c + Math.sqrt(x), 0));
-
-/* function funcs(str) {
-    document.write(str);
-}
-
-function riri(a, b) {
-    return a*b/3;
-}*/
-
-//funcs("kkk = " + riri(3, 5));
-
-
-function ds() {
-    let d = new Date();
-
-    document.write(d.getFullYear() + "-" + d.getMonth() + "-" + d.getDate())
-}
-ds()
