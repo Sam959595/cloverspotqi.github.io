19b1a104a0463fae05ae9c186fd5ebfe845d94c2
Sam959595 <sambertovv@gmail.com>
2021-01-22 07:34:30 +0500

    fisrt commit

---
+++ js/example/post.js
@@ -0,0 +1,27 @@
+async function sech() {
+    let t = await fetch('json/post.json'),
+    b = await t.json(),
+    e = document.querySelector('.includes ul');
+
+    b = b.splice(0, 5);
+
+    for (let i = 0; i < b.length; i++) {
+        e.innerHTML += `<li><a href="${b[i].href}"><div style="background-image: url(${b[i].image})"></div><div><p>${b[i].date}</p><h5>${b[i].title}</h5></div></a></li>`;
+    }
+
+    $(window).scroll(function() {
+        if ($(window).scrollTop() + $(window).height() >= $(document).height()) {
+            console.log('call');
+        };
+    });
+};
+sech();
+
+//
+
+window.onload = function() {
+    let a = $('blockquote p'),
+    b = 'quote';
+
+    a[0].classList.add(b);
+};
---
+++ js/example/search.js
@@ -0,0 +1,68 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let s = $('html'),
+    d = $('body'),
+    a = $('main li:last-child'),
+    b = $('aside'),
+    e = $('aside input'),
+    i = $('aside > div:first-child div:first-child'),
+    c = $('aside > div:first-child div:first-child span'),
+    r = $('aside > div:first-child div:last-child span'),
+    p = 'no-scroll',
+    v = 'visible',
+    h = 'hide',
+    x = 'disappear';
+
+    a.click(function() {
+        
+        b[0].classList.add(v);
+        s[0].classList.add(p);
+        d[0].classList.add(p);
+    });
+    c.click(function() {
+
+        b[0].classList.remove(v);
+        s[0].classList.remove(p);
+        d[0].classList.remove(p);
+    });
+
+    e.click(function() {
+
+        r[0].classList.add(h);
+        i[0].classList.add(x);
+    });
+    r.click(function() {
+
+        r[0].classList.remove(h);
+        i[0].classList.remove(x);
+    });
+});
+
+//
+
+async function categories() {
+    let t = await fetch('json/nav.json'),
+    i = await t.json(),
+    r = null,
+    e = document.querySelector('aside > div:last-child ul:first-child');
+
+    for (r in i) {
+
+        e.innerHTML += `<li><a href="${i[r].href}">${i[r].title}</a></li>`;
+    };
+};
+categories();
+
+async function categDown() {
+    let t = await fetch('json/post.json'),
+    i = await t.json(),
+    r = null,
+    e = document.querySelector('aside > div:last-child ul:last-child');
+
+    i = i.splice(0, 5);
+
+    for (r in i) {
+
+        e.innerHTML += `<li><a href="${i[r].href}"><div><div style="background-image: url(${i[r].image_mini})"></div></div><div><p>${i[r].date}</p><h5>${i[r].title}</h5></div></a></li>`;
+    };
+};
+categDown();
