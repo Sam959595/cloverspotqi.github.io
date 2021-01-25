97974c9d8809cf5c569ee3a76df2892df2727744
Sam959595 <sambertovv@gmail.com>
2021-01-26 01:25:36 +0500

    fisrt commit

--- js/index/post.js
+++ js/index/post.js
@@ -1,9 +1,9 @@
 async function sech() {
-    let t = await fetch('json/post.json'),
+    let t = await fetch('json/general.json'),
     b = await t.json(),
-    e = document.querySelector('.includes ul');
-  
-    b = b.splice(0, 5);
+    e = $('.general ul')[0];
+
+    b = b.splice(0, 8);
   
     for (let i = 0; i < b.length; i++) {
         e.innerHTML +=
@@ -29,3 +29,29 @@ async function sech() {
     });
 };
 sech();
+
+//
+
+async function sef() {
+    let t = await fetch('json/policy.json'),
+    b = await t.json(),
+    e = $('.policy ul')[0];
+
+    b = b.splice(0, 8);
+  
+    for (i in b) {
+        e.innerHTML +=
+        `<li>
+            <a href='${b[i].href}'>
+                <div>
+                    <time datetime='${b[i].date}'></time>
+                    <h5>${b[i].title}</h5>
+                </div>
+                <div>
+                    <div style='background-image: url(${b[i].image})'></div>
+                </div>
+            </a>
+        </li>`;
+    };
+};
+sef();
