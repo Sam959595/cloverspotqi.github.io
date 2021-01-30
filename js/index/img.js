1b79231025ddf0cf160d313947d8d6b546e0832a
Sam959595 <sambertovv@gmail.com>
2021-01-30 07:00:32 +0500

    fisrt commit

---
+++ js/index/img.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+    let tt = document.querySelectorAll('[data-style]');
+    
+    function vv(a) {
+        let u = a.getAttribute('data-style');
+        
+        if (!u) {
+            return;
+        }
+        a.style = u
+    };
+    
+    let bb = {
+        threshold: 0,
+        rootMargin: '0px'
+    };
+    
+    const ee = new IntersectionObserver((a, b) => {
+        a.forEach(c => {
+            if (!c.isIntersecting) {
+                return;
+            } else {
+                vv(c.target);
+                b.unobserve(c.target);
+            }
+        });
+    }, bb);
+    
+    tt.forEach(a => {
+        ee.observe(a);
+    });
+});
+
+/*u.forEach((e) => {
+    e.setAttribute('style', e.getAttribute('data-style'));
+    e.removeAttribite('data-style');
+});*/
