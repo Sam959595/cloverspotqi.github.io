800211edb7becca88dc54c18ab979df6803ad5ce
Sam959595 <sambertovv@gmail.com>
2021-01-31 09:08:19 +0500

    gisrt commit

--- js/index/img.js
+++ js/index/img.js
@@ -1,35 +1,33 @@
-$(document).ready(function() {
-    let tt = document.querySelectorAll('[data-style]');
+let tt = document.querySelectorAll('[data-style]');
     
-    function vv(a) {
-        let u = a.getAttribute('data-style');
+function vv(a) {
+    let u = a.getAttribute('data-style');
         
-        if (!u) {
+    if (!u) {
+        return;
+    };
+    a.style = u;
+    a.removeAttribute('data-style');
+};
+    
+let bb = {
+    threshold: 0,
+    rootMargin: '0px'
+};
+    
+let ee = new IntersectionObserver((a, b) => {
+    a.forEach(c => {
+         if (!c.isIntersecting) {
             return;
+        } else {
+            vv(c.target);
+            b.unobserve(c.target);
         };
-        a.style = u;
-        a.removeAttribute('data-style');
-    };
-    
-    let bb = {
-        threshold: 0,
-        rootMargin: '0px'
-    };
-    
-    let ee = new IntersectionObserver((a, b) => {
-        a.forEach(c => {
-            if (!c.isIntersecting) {
-                return;
-            } else {
-                vv(c.target);
-                b.unobserve(c.target);
-            };
-        });
-    }, bb);
-    
-    tt.forEach(a => {
-        ee.observe(a);
     });
+}, bb);
+    
+tt.forEach(a => {
+    ee.observe(a);
 });
 
 /*u.forEach((e) => {
