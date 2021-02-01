62c3098b0a0491a4df3fc7e92d3587f23fe61d16
Sam959595 <sambertovv@gmail.com>
2021-02-01 10:04:06 +0500

    fisrt commit

--- js/index/img.js
+++ js/index/img.js
@@ -1,33 +1,35 @@
-let tt = document.querySelectorAll('[data-style]');
+$(document).ready(function() {
+    let tt = document.querySelectorAll('[data-style]');
+    
+    function vv(a) {
+        let u = a.getAttribute('data-style');
 
-function vv(a) {
-    let u = a.getAttribute('data-style');
-        
-    if (!u) {
-        return;
-    };
-    a.style = u;
-    a.removeAttribute('data-style');
-};
-
-let bb = {
-    threshold: 0,
-    rootMargin: '0px'
-};
-
-let ee = new IntersectionObserver((a, b) => {
-    a.forEach(c => {
-         if (!c.isIntersecting) {
+        if (!u) {
             return;
-        } else {
-            vv(c.target);
-            b.unobserve(c.target);
         };
+        a.style = u;
+        a.removeAttribute('data-style')
+    };
+    
+    let bb = {
+        threshold: 0,
+        rootMargin: '0px'
+    };
+    
+    let ee = new IntersectionObserver((a, b) => {
+        a.forEach(c => {
+            if (!c.isIntersecting) {
+                return;
+            } else {
+                vv(c.target);
+                b.unobserve(c.target);
+            };
+        });
+    }, bb);
+    
+    tt.forEach(a => {
+        ee.observe(a);
     });
-}, bb);
-
-tt.forEach(a => {
-    ee.observe(a);
 });
 
 /*u.forEach((e) => {
--- js/index/post.js
+++ js/index/post.js
@@ -1,3 +1,25 @@
+async function tt() {
+    let t = await fetch('json/relatively.json'),
+    b = await t.json(),
+    e = $('.reference ul');
+
+    b = b.splice(0, 3);
+  
+    for (i in b) {
+        e[0].innerHTML +=
+        `<li>
+            <a href='${b[i].href}'>
+                <div data-style='background-image: url(${b[i].image})' style='background-image: url(#)'></div>
+                <div>
+                    <h5>${b[i].title}</h5>
+                    <p>${b[i].details}</p>
+                </div>
+            </a>
+        </li>`;
+    };
+};
+tt();
+
 async function bb() {
     let t = await fetch('json/general.json'),
     b = await t.json(),
