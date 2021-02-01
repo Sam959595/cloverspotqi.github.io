775a46496b9df12f026e48ecd59af5fd5c017d95
Sam959595 <sambertovv@gmail.com>
2021-02-01 05:43:58 +0500

    fisrt commit

--- js/index/banner.js
+++ js/index/banner.js
@@ -3,16 +3,4 @@ window.onload = function() {
     b = 'quote';
 
     a[0].classList.add(b);
-
-    //
-
-    function tt() {
-        let bb = $('.reference ul a')[0],
-        vv = $('.includes ul a > div:last-child')[0].offsetWidth;
-
-        for (let i = 0; i < bb.length; i++) {
-            bb[i].style.width = vv + 'px';
-        }
-    }
-    tt();
 };
