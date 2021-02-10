d0a58084fb5a04c4b84ef516790466d7be15575b
Sam959595 <sambertovv@gmail.com>
2021-02-10 19:54:12 +0500

    fisrt commit

--- js/index/post.js
+++ js/index/post.js
@@ -33,7 +33,7 @@ async function bb() {
             <a href='${b[i].href}'>
                 <div>
                     <time datetime='${b[i].date}'></time>
-                    <h5>${b[i].title}</h5>
+                    <h5>${b[i].title}<p>${b[i].details}</p></h5>
                 </div>
                 <div>
                     <div data-style='background-image: url(${b[i].image})' style='background-image: url(#)'></div>
