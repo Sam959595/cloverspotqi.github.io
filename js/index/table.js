0c2242cfe2b031fb71e5083d3f6d6b29f9194a42
Sam959595 <sambertovv@gmail.com>
2021-02-20 03:25:42 +0500

    fisrt commit

---
+++ js/index/table.js
@@ -0,0 +1,47 @@
+async function bb() {
+    let t = await fetch('json/general.json'),
+    b = await t.json(),
+    e = $('.general ul');
+
+    b = b.splice(8, 10);
+  
+    for (i in b) {
+        e[0].innerHTML +=
+        `<li>
+            <a href='${b[i].href}'>
+                <div>
+                    <time datetime='${b[i].date}'></time>
+                    <h5>${b[i].title}</h5>
+                </div>
+                <div>
+                    <div data-style='background-image: url(${b[i].image})' style='background-image: url(#)'></div>
+                </div>
+            </a>
+        </li>`;
+    };
+};
+bb();
+
+async function vv() {
+    let t = await fetch('json/policy.json'),
+    b = await t.json(),
+    e = $('.policy ul');
+
+    b = b.splice(8, 10);
+  
+    for (i in b) {
+        e[0].innerHTML +=
+        `<li>
+            <a href='${b[i].href}'>
+                <div>
+                    <time datetime='${b[i].date}'></time>
+                    <h5>${b[i].title}</h5>
+                </div>
+                <div>
+                    <div data-style='background-image: url(${b[i].image})' style='background-image: url(#)'></div>
+                </div>
+            </a>
+        </li>`;
+    };
+};
+vv();
