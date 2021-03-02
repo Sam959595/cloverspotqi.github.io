058ce5eef2a00ce56e9da1220f988b2f8f7f666f
Sam959595 <sambertovv@gmail.com>
2021-03-02 21:24:32 +0500

    fisrt commit

--- js/index/table.js
+++ js/index/table.js
@@ -10,8 +10,8 @@ async function bb() {
         `<li>
             <a href='${b[i].href}'>
                 <div>
-                    <time datetime='${b[i].date}'></time>
                     <h5>${b[i].title}</h5>
+                    <time datetime='${b[i].date}'></time>
                 </div>
                 <div>
                     <div data-style='background-image: url(${b[i].image})' style='background-image: url(#)'></div>
