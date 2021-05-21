10241eedc700b7c522b873248d645a50fb242adc
Sam959595 <sambertovv@gmail.com>
2021-05-22 00:27:26 +0500

    fisrt commit

--- js/order/add-account.js
+++ js/order/add-account.js
@@ -1,3 +1,8 @@
-let a = $('input[type="email]"')[0],
-b = $('input[type="text"]')[0],
-c = $('section.pp div input[type="password"')[0];
+function bb() {
+    let a = $('section.pp div:nth-child(1) input'),
+    b = $('section.pp div:nth-child(2) input')[0],
+    c = $('section.pp div:nth-child(3) input:nth-child(2)')[0],
+    d = $('section.pp div:nth-child(3) input:nth-child(3)')[0];
+    
+    a.style('background-color : red');
+}
