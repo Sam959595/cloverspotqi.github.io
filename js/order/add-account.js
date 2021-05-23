65f9b60c95c589d5376e09da11946345fe91df03
Sam959595 <sambertovv@gmail.com>
2021-05-24 03:51:48 +0500

    fisrt commit

--- js/order/add-account.js
+++ js/order/add-account.js
@@ -1,11 +1,12 @@
-$(document).ready(function () {
-    let a = $('section.bb input:nth-of-type(1)'),
-    b = $('section.bb input:nth-of-type(2)')[0],
-    n = $('section.bb hr:nth-of-type(2)')[0],
+$(window).ready (function () {
+    let a = $ ('section.bb input:nth-of-type(1)'),
+    b = $ ('section.bb input:nth-of-type(2)') [0],
+    n = $ ('section.bb hr:nth-of-type(2)') [0],
+    v = $ ('section.gg'),
     c = 'ee',
     r = 'rr';
 
-    a.keydown(function(e) {
+    a.keydown (function (e) {
 
         if (e.keyCode === 13) {
 
@@ -15,6 +16,22 @@ $(document).ready(function () {
                 b.classList.add(c);
                 n.classList.add(r);
             }
-        }
+        };
     });
 });
+
+function ss () {
+    let a = $ ('section.bb input:nth-of-type(1)'),
+    b = $ ('section.bb input:nth-of-type(2)') [0],
+    v = $ ('section.gg') [0],
+    n = (a || b),
+    k = (a && b);
+
+    n.keydown (function (e) {
+
+        if (e.keyCode === 13 && k.value.length > 0) {
+            v.style.display = 'block';
+        }
+    });
+}
+ss ();
