ffdcef4aee1292a93d16e816c8253fd2e8f6b2c8
Sam959595 <sambertovv@gmail.com>
2021-05-27 03:30:36 +0500

    fisrt commit

--- js/order/add-account.js
+++ js/order/add-account.js
@@ -2,13 +2,15 @@ let nn = () => {
     let a = $ ('section.bb input:nth-of-type(1)')[0],
     b = $ ('section.bb input:nth-of-type(2)')[0],
     n = $ ('section.bb input'),
-    v = $ ('blockquote.gg'),
+    v = $ ('section.gg'),
     p = $ ('section.bb hr:nth-of-type(2)')[0],
     c = 'ee',
     r = 'rr',
     y = '<h6>Введите ключ</h6>',
     q = '<h6>Введите пароль</h6>',
-    u = '<h6>Введите ключ и пароль</h6>';
+    u = '<h6>Введите ключ и пароль</h6>',
+    e = '<h6>Неверный ключ или пароль</h6>',
+    g = '<h6>Сбой авторизации</h6>';
 
     n.keydown ( (x) => {
         let k = (x.keyCode === 13);
