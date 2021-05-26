c620fb6b844c6fb77f3d14103bd8aed73baeb9db
Sam959595 <sambertovv@gmail.com>
2021-05-27 01:00:07 +0500

    fisrt commit

--- js/order/add-account.js
+++ js/order/add-account.js
@@ -1,50 +1,43 @@
-let ff = () => {
-    let a = $ ('section.bb input:nth-of-type(1)'),
-    b = $ ('section.bb input:nth-of-type(2)') [0],
-    n = $ ('section.bb hr:nth-of-type(2)') [0],
-    v = $ ('section.gg'),
-    c = 'ee',
-    r = 'rr';
-
-    a.keydown (function (x) {
-
-        if (x.keyCode === 13) {
-
-            if (this.value.length === 0) {
-                return false
-            } else {
-                b.classList.add(c);
-                n.classList.add(r)
-            }
-        }
-    })
-}
-
-ff();
-
 let nn = () => {
-    let w = localStorage.getItem('username'),
-    t = localStorage.getItem('passwork'),
-    a = $ ('section.bb input:nth-of-type(1)'),
+    let a = $ ('section.bb input:nth-of-type(1)'),
     b = $ ('section.bb input:nth-of-type(2)'),
     n = $ ('section.bb input'),
-    v = $ ('section.gg');
+    v = $ ('section.gg'),
+    p = $ ('section.bb hr:nth-of-type(2)'),
+    c = 'ee',
+    r = 'rr',
+    y = '<h6>Введите ключ</h6>',
+    q = '<h6>Введите пароль</h6>',
+    u = '<h6>Введите ключ и пароль</h6>';
 
     n.keydown ( (x) => {
-        let m = (x.keyCode === 13);
+        let k = (x.keyCode === 13);
         
-        if (m && (a[0].value.length === 0) && (b.value != 0)) {
-            v.html('<h6>Введите ключ</h6>')
-        } else if (m && (a[0].value != 0) && (b[0].value.length === 0)) {
-            v.html('<h6>Введите пароль</h6>')
-        } else if (m && (n[0].value.length === 0)) {
-            v.html('<h6>Введите ключ и пароль</h6>')
-        } else if (m && (n[0].value != 0)) {
-            v.html('')
+        if (k && a[0].value.length === 0) {
+            v.html(u)
+        } else if (k && a[0].value.length != 0) {
+            p[0].classList.add(r);
+            b[0].classList.add(c)
         } else {
-            v.html('<h6>Неверный ключ или пароль</h6>')
+            v.html('')
+        }
+
+        if (k && a[0].value.length === 0 && b[0].value.length != 0) {
+            v.html(y)
+        } else if (k && b[0].value.length === 0 && a[0].value.length) {
+            v.html(q)
         }
     });
+
+    ( () => {
+        let w = localStorage.getItem('username'),
+        t = localStorage.getItem('passwork');
+
+        $.post('user.php', {
+            w: username,
+            t: password
+        });
+    })
 }
 
 nn();
