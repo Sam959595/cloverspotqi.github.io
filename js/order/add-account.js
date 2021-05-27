337df8c21e0aa4665da794b242480279dcc6dac8
Sam959595 <sambertovv@gmail.com>
2021-05-27 06:07:46 +0500

    fisrt commit

--- js/order/add-account.js
+++ js/order/add-account.js
@@ -11,38 +11,39 @@ let nn = () => {
     u = '<h6>Введите ключ и пароль</h6>',
     t = '<svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 100 100" fill="var(--red-2)">\
     <g transform="rotate(0 50 50)">\
-      <rect x="47.5" y="28" rx="2.1" ry="2.1" width="5" height="14">\
-        <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="0.5681818181818182s" begin="-0.4971590909090909s" repeatCount="indefinite"/>\
+      <rect x="46" y="19" rx="3.96" ry="3.96" width="8" height="22">\
+        <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.875s" repeatCount="indefinite"></animate>\
       </rect>\
     </g><g transform="rotate(45 50 50)">\
-      <rect x="47.5" y="28" rx="2.1" ry="2.1" width="5" height="14">\
-        <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="0.5681818181818182s" begin="-0.42613636363636365s" repeatCount="indefinite"/>\
+      <rect x="46" y="19" rx="3.96" ry="3.96" width="8" height="22">\
+        <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.75s" repeatCount="indefinite"></animate>\
       </rect>\
     </g><g transform="rotate(90 50 50)">\
-      <rect x="47.5" y="28" rx="2.1" ry="2.1" width="5" height="14">\
-        <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="0.5681818181818182s" begin="-0.35511363636363635s" repeatCount="indefinite"/>\
+      <rect x="46" y="19" rx="3.96" ry="3.96" width="8" height="22">\
+        <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.625s" repeatCount="indefinite"></animate>\
       </rect>\
     </g><g transform="rotate(135 50 50)">\
-      <rect x="47.5" y="28" rx="2.1" ry="2.1" width="5" height="14">\
-        <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="0.5681818181818182s" begin="-0.2840909090909091s" repeatCount="indefinite"/>\
+      <rect x="46" y="19" rx="3.96" ry="3.96" width="8" height="22">\
+        <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.5s" repeatCount="indefinite"></animate>\
       </rect>\
     </g><g transform="rotate(180 50 50)">\
-      <rect x="47.5" y="28" rx="2.1" ry="2.1" width="5" height="14">\
-        <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="0.5681818181818182s" begin="-0.21306818181818182s" repeatCount="indefinite"/>\
+      <rect x="46" y="19" rx="3.96" ry="3.96" width="8" height="22">\
+        <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.375s" repeatCount="indefinite"></animate>\
       </rect>\
     </g><g transform="rotate(225 50 50)">\
-      <rect x="47.5" y="28" rx="2.1" ry="2.1" width="5" height="14">\
-        <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="0.5681818181818182s" begin="-0.14204545454545456s" repeatCount="indefinite"/>\
+      <rect x="46" y="19" rx="3.96" ry="3.96" width="8" height="22">\
+        <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.25s" repeatCount="indefinite"></animate>\
       </rect>\
     </g><g transform="rotate(270 50 50)">\
-      <rect x="47.5" y="28" rx="2.1" ry="2.1" width="5" height="14">\
-        <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="0.5681818181818182s" begin="-0.07102272727272728s" repeatCount="indefinite"/>\
+      <rect x="46" y="19" rx="3.96" ry="3.96" width="8" height="22">\
+        <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.125s" repeatCount="indefinite"></animate>\
       </rect>\
     </g><g transform="rotate(315 50 50)">\
-      <rect x="47.5" y="28" rx="2.1" ry="2.1" width="5" height="14">\
-        <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="0.5681818181818182s" begin="0s" repeatCount="indefinite"/>\
+      <rect x="46" y="19" rx="3.96" ry="3.96" width="8" height="22">\
+        <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="0s" repeatCount="indefinite"></animate>\
       </rect>\
-    </g></svg>',
+    </g>\
+    </svg>',\
     e = '<h6>Неверный ключ или пароль</h6>',
     g = '<h6>Сбой авторизации</h6>';
 
