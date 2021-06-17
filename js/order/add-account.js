8f94ae4e4011048446a95c5c70371f73befe16fa
Your NameAdmin@This-PC MINGW64 ~/Downloads/snippets (master)$ git config --global user.email you@example.comAdmin@This-PC MINGW64 ~/Downloads/snippets (master)$   git config --global user.name Your <you@example.com>
2021-06-17 11:30:42 +0500

    fisrt commit

--- js/order/add-account.js
+++ js/order/add-account.js
@@ -3,9 +3,6 @@ let nn = () => {
     b = $ ('section.bb input:nth-of-type(2)')[0],
     n = $ ('section.bb input'),
     v = $ ('section.gg'),
-    p = $ ('section.bb hr:nth-of-type(2)')[0],
-    c = 'ee',
-    r = 'rr',
     y = '<h6>Введите ключ</h6>',
     q = '<h6>Введите пароль</h6>',
     u = '<h6>Введите ключ и пароль</h6>',
@@ -51,17 +48,14 @@ let nn = () => {
         let k = (x.keyCode === 13);
 
         if (k && (n[0].value.length === 0)) {
-            v.html(u)
-        } else if (k && (p.classList.contains(r)) && (a.value.length != 0) && (b.value.length === 0)) {
-            v.html(q)
-        } else if (k && (a.value.length != 0)) {
-            p.classList.add(r);
-            b.classList.add(c)
+          v.html(u)
         } else {
-            v.html('')
+          v.html('')
         }
-
-        if (k && (a.value.length === 0) && (b.value.length != 0)) {
+        
+        if (k && (b.value.length === 0) && (a.value.length != 0)) {
+          v.html(q)
+        } else if (k && (a.value.length === 0) && (b.value.length != 0)) {
             v.html(y);
         } else if (k && (b.value.length != 0) && (a.value.length != 0)) {
             v.html(t);
