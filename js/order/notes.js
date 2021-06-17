2d90b7b12e3b7fc67c9deb94b4337738c7d6de7b
Your NameAdmin@This-PC MINGW64 ~/Downloads/snippets (master)$ git config --global user.email you@example.comAdmin@This-PC MINGW64 ~/Downloads/snippets (master)$   git config --global user.name Your <you@example.com>
2021-06-18 01:24:17 +0500

    fisrt commit

--- js/order/notes.js
+++ js/order/notes.js
@@ -1,3 +1,16 @@
+let ss = () => {
+    let pp = $('section.pp ul'),
+    vv = $('section.vv'),
+    tt = '<p>Нет добавленных заметок</p>';
+
+    if (pp[0].classList.contains('li')) {
+        return true
+    } else {
+        vv.html(tt)
+    }
+}
+ss();
+
 $('section.pp button').click( (x) => {
     x.preventDefault();
     $('section.pp li')[0].remove();
