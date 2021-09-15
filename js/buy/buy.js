abb4fa699a9b68d9b4fc6e82ab2e9b935c2b7219
Sam959595 <69326235+Sam959595@users.noreply.github.com>
2021-09-15 21:18:03 +0300

    Update buy.js

--- js/buy/buy.js
+++ js/buy/buy.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', function () {
     let h = $('html'),
     b = $('body'),
     o = $('apple-pay-button'),
