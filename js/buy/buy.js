c8a13800aaa76e6196cbcd10789641a0372c8996
Sam959595 <69326235+Sam959595@users.noreply.github.com>
2021-09-15 18:40:54 +0300

    Update buy.js

--- js/buy/buy.js
+++ js/buy/buy.js
@@ -1,18 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
     let h = $('html'),
     b = $('body'),
-    a = $('section.rr button'),
+    o = $('section.rr button'),
     z = $('section.zz'),
-    c = $('section.zz div button:last-child'),
+    e = $('section.zz div button:last-child'),
     n = 'no-scroll',
     v = 'visible';
 
-    a.click(function() {
+    o.click(function() {
         z[0].classList.add(v);
         h[0].classList.add(n);
         b[0].classList.add(n);
     });
-    c.click(function() {
+    e.click(function() {
         z[0].classList.remove(v);
         h[0].classList.remove(n);
         b[0].classList.remove(n);
