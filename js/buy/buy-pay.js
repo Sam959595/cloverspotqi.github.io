537cad0af51f48e4ff58d78fc74f5308b8ebe455
Sam959595 <69326235+Sam959595@users.noreply.github.com>
2021-09-17 19:55:48 +0300

    Update buy-pay.js

--- js/buy/buy-pay.js
+++ js/buy/buy-pay.js
@@ -1,1 +1,14 @@
+if (window.ApplePaySession) {
+   let p = {
+       total: {
+           label: 'Магазин',
+           amount: 50.00
+       },
+       countryCode: 'RU',
+   currencyCode: 'RUB',
+   merchantCapabilities: ['supports3DS'],
+   supportedNetworks: ['masterCard', 'visa']
+   };
 
+   let b = new window.ApplePaySession(1, p);
+}
