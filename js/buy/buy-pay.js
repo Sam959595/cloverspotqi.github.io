5e6dcbb05401cdbe640e94f40269575491ef93a2
Sam959595 <69326235+Sam959595@users.noreply.github.com>
2021-09-17 20:06:54 +0300

    Update buy-pay.js

--- js/buy/buy-pay.js
+++ js/buy/buy-pay.js
@@ -1,4 +1,7 @@
 if (window.ApplePaySession) {
+   let n = $('section.zz div apple-pay-button');
+
+   n.click ( () => {
    let p = {
        total: {
            label: 'Магазин',
@@ -11,4 +14,5 @@ if (window.ApplePaySession) {
    };
 
    let b = new window.ApplePaySession(1, p);
+   };
 }
