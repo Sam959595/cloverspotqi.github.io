84f3f60cb14061858002a4dc0e85f47a051e7a8f
Sam959595 <69326235+Sam959595@users.noreply.github.com>
2021-09-18 07:39:03 +0300

    Update buy-pay.js

--- js/buy/buy-pay.js
+++ js/buy/buy-pay.js
@@ -1,3 +1,36 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let o = $('section.bb div button:last-child'),
+    e = $('section.zz div button:last-child'),
+    h = $('html'),
+    b = $('body'),
+    z = $('section.zz'),
+    n = 'no-scroll',
+    v = 'visible';
+
+    o.click( () => {
+        h[0].classList.add(n);
+        b[0].classList.add(n);
+        z[0].classList.add(v);
+    });
+    e.click( () => {
+        h[0].classList.remove(n);
+        b[0].classList.remove(n);
+        z[0].classList.remove(v);
+    });
+
+    let p = $('section.zz li:last-child'),
+        j = $('section.zz li:last-child h6'),
+        f = 'active';
+
+    p.click( () => {
+        if (j[0].classList.contains(f) == false) {
+            j[0].classList.add(f);
+            } else {
+                  j[0].classList.remove(f);
+         }
+    });
+});
+
 if (window.ApplePaySession) {
    let n = $('section.zz div button:first-child');
 
