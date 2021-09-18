aa1241f7a42df7d77cfc66f6ed388a686c8ab37d
Sam959595 <69326235+Sam959595@users.noreply.github.com>
2021-09-18 07:34:41 +0300

    Update buy.js

--- js/buy/buy.js
+++ js/buy/buy.js
@@ -1,32 +1,3 @@
-document.addEventListener('DOMContentLoaded', () => {
-    let o = $('apple-pay-button'),
-    e = $('section.zz div button:last-child'),
-    h = $('html'),
-    b = $('body'),
-    z = $('section.zz'),
-    n = 'no-scroll',
-    v = 'visible';
-
-    o.click( () => {
-        h[0].classList.add(n);
-        b[0].classList.add(n);
-        z[0].classList.add(v);
-    });
-    e.click( () => {
-        h[0].classList.remove(n);
-        b[0].classList.remove(n);
-        z[0].classList.remove(v);
-    });
-
-    let p = $('section.zz li:last-child'),
-        j = $('section.zz li:last-child h6'),
-        f = 'active';
-
-    p.click( () => {
-        if (j[0].classList.contains(f) == false) {
-            j[0].classList.add(f);
-            } else {
-                  j[0].classList.remove(f);
-         }
-    });
-});
+( () => {
+     let a = $('section.hh');
+}
