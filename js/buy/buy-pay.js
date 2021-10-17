5c1160d0308c8dd65db07d23020def24996e836d
Sam959595 <69326235+Sam959595@users.noreply.github.com>
2021-10-17 07:49:17 +0300

    Update buy-pay.js

--- js/buy/buy-pay.js
+++ js/buy/buy-pay.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function () = {
+document.addEventListener('DOMContentLoaded', function () {
     let a = $('dl dd.string'),
     b = $('dl dd.string div'),
     c = $('dl dd.string a');
