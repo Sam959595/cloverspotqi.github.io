83c04ca64d039c89285067c1c942c36cfdd6fef0
Sam <69326235+Sam959595@users.noreply.github.com>
2022-05-20 17:45:19 +0300

    Update tv.js

--- js/tv/tv.js
+++ js/tv/tv.js
@@ -16,3 +16,14 @@ document.addEventListener('DOMContentLoaded', function () {
         this.remove();
     });
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    let a = $('section.ww hr'),
+        b = $('section.ww textarea');
+
+    if (b.text.length > 500) {
+        a.css('opacity', '1')
+    } else {
+        a.css('opacity', '0')
+    };
+});
