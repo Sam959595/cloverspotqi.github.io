12b4acf733f57897cafc382920947bf650e95ed7
Sam <69326235+Sam959595@users.noreply.github.com>
2022-05-21 13:33:10 +0300

    Update tv.js

--- js/tv/tv.js
+++ js/tv/tv.js
@@ -22,9 +22,9 @@ document.addEventListener('keyup', () => {
     b = $('section.ww hr'),
     c = 12;
 
-    if (a.value.split('\n').length > c) {
+    if (a.value.split('\n').length >= c) {
         b.css('opacity', '1')
-    } else if (a.value.split('\n').length < c) {
+    } else if (a.value.split('\n').length <= c) {
         b.css('opacity', '0')
     };
 });
