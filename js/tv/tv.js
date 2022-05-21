92a51fe0572a1fe17a21d1b5a523b9f1d6ed37b9
Sam <69326235+Sam959595@users.noreply.github.com>
2022-05-21 14:41:41 +0300

    Update tv.js

--- js/tv/tv.js
+++ js/tv/tv.js
@@ -18,11 +18,10 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 document.addEventListener('keyup', () => {
-    let a = $('section.ww textarea')[0].scrollHeight,
-    b = $('section.ww hr'),
-    c = 279;
+    let a = $('section.ww textarea')[0],
+    b = $('section.ww hr');
 
-    if (a > c) {
+    if (a.scrollHeight > a.clientHeight) {
         b.css('opacity', '1')
     } else {
         b.css('opacity', '0')
