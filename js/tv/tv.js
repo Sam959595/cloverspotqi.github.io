99de68ee7f3afe3001651c8ca5017a49bc2852f9
Sam <69326235+Sam959595@users.noreply.github.com>
2022-06-26 11:43:28 +0300

    Update tv.js

--- js/tv/tv.js
+++ js/tv/tv.js
@@ -2,7 +2,7 @@ window.addEventListener('DOMContentLoaded', () => {
     let a = $('.string'),
     b = $('.string div');
 
-    if (b.height() > '48') {
+    if (b.height() > '60') {
         a[0].classList.add('hide');
 
         b = document.createElement('span');
