ab09b1c957c5ba0554d63c2145f2d25c6ce7721c
Sam <69326235+Sam959595@users.noreply.github.com>
2022-07-26 23:28:57 +0300

    Update book.js

--- js/book/book.js
+++ js/book/book.js
@@ -5,3 +5,24 @@ window.addEventListener('DOMContentLoaded', () => {
     document.head.append(mm)
     mm.innerHTML = ':root{--screen:' + bb + 'px}'
 })
+
+window.addEventListener('DOMContentLoaded', () => {
+    let a = $('.string')[0],
+    b = $('.string h6')
+
+    if (b.height() > '57') {
+        a.classList.add('hide')
+
+        b = document.createElement('span')
+
+        b.innerHTML = 'продолжить'
+        a.appendChild(b)
+    } else {
+        return false
+    }
+    
+    $('.string span').click( () => {
+        a.classList.remove('hide')
+        $('.string span').remove()
+    })
+})
