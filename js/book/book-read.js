706c6f66207e48469c543873f74f622b08934397
Sam <69326235+Sam959595@users.noreply.github.com>
2022-08-06 15:09:10 +0300

    Update book-read.js

--- js/book/book-read.js
+++ js/book/book-read.js
@@ -35,16 +35,16 @@ window.addEventListener('change', () => {
     nn = $('section.vv')
 
     if (vv == '1') {
-        nn[0].classList.add('yyu')
-        nn[0].classList.remove('yyo', 'yyy')
-    } else if (vv == '2') {
-        nn[0].classList.remove('yyu', 'yyo', 'yyy')
-    } else if (vv == '3') {
-        nn[0].classList.add('yyy')
-        nn[0].classList.remove('yyu', 'yyo')
-    } else if (vv == '0') {
         nn[0].classList.add('yyo')
         nn[0].classList.remove('yyu', 'yyy')
+    } else if (vv == '2') {
+        nn[0].classList.add('yyu')
+        nn[0].classList.remove('yyo', 'yyy')
+    } else if (vv == '3') {
+        nn[0].classList.remove('yyu', 'yyo', 'yyy')
+    } else if (vv == '4') {
+        nn[0].classList.add('yyy')
+        nn[0].classList.remove('yyu', 'yyo')
     }
 })
 
@@ -52,27 +52,36 @@ window.addEventListener('change', () => {
     let vv = $('#nn').val(),
     nn = $('section.vv')
 
-    if (vv == '0') {
-        nn[0].classList.add('sss')
-    } else {
+    if (vv == '1') {
+        nn[0].classList.add('zzz')
         nn[0].classList.remove('sss')
+    } else if (vv == '2') {
+        nn[0].classList.add('sss')
+        nn[0].classList.remove('zzz')
+    } else if (vv == '3') {
+        nn[0].classList.remove('sss', 'zzz')
     }
 })
 
 window.addEventListener('change', () => {
     let vv = $('#ee').val(),
-    nn = $('body')
+    nn = $('body')[0]
 
-    if (vv == '0') {
-        nn[0].classList.add('rrr')
-    } else {
-        nn[0].classList.remove('rrr')
-    }
+    if (vv == '1') {
+        nn.classList.add('rrr')
+        nn.classList.remove('ttt')
 
-    if (vv == '0') {
+        $('meta[media="(prefers-color-scheme: light)"]').attr('content', 'white')
+        $('meta[media="(prefers-color-scheme: dark)"]').attr('content', 'black')
+    } else if (vv == '2') {
+        nn.classList.add('ttt')
+        nn.classList.remove('rrr')
+
         $('meta[media="(prefers-color-scheme: light)"]').attr('content', '#feddbc')
         $('meta[media="(prefers-color-scheme: dark)"]').attr('content', '#120b08')
-    } else {
+    } else if (vv == '3') {
+        nn.classList.remove('rrr', 'ttt')
+
         $('meta[media="(prefers-color-scheme: light)"]').attr('content', '#f2f2f9')
         $('meta[media="(prefers-color-scheme: dark)"]').attr('content', '#1a1a1c')
     }
