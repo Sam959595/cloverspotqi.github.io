aa45423c97808021e974440324c9d73a9a8a992f
Sam <69326235+Sam959595@users.noreply.github.com>
2022-08-23 15:31:02 +0300

    Update chat.js

--- js/chat/chat.js
+++ js/chat/chat.js
@@ -1,8 +1,10 @@
-document.querySelectorAll('textarea').forEach(el => {
-  el.style.height = el.setAttribute('style', 'height: ' + el.scrollHeight + 'px');
-  el.classList.add('auto');
-  el.addEventListener('input', e => {
-    el.style.height = 'auto';
-    el.style.height = (el.scrollHeight) + 'px';
-  });
+window.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('textarea').forEach(el => {
+        el.style.height = el.setAttribute('style', 'height: ' + el.scrollHeight + 'px');
+        el.classList.add('auto');
+        el.addEventListener('input', e => {
+            el.style.height = 'auto';
+            el.style.height = (el.scrollHeight) + 'px';
+        });
+    });
 })
