6eaa063967ec81c67e6135ca45668d66d405d4c5
Sam <69326235+Sam959595@users.noreply.github.com>
2022-08-23 13:44:13 +0300

    Create chat.js

---
+++ js/chat/chat.js
@@ -0,0 +1,8 @@
+document.querySelectorAll('textarea').forEach(el => {
+  el.style.height = el.setAttribute('style', 'height: ' + el.scrollHeight + 'px');
+  el.classList.add('auto');
+  el.addEventListener('input', e => {
+    el.style.height = 'auto';
+    el.style.height = (el.scrollHeight) + 'px';
+  });
+});
