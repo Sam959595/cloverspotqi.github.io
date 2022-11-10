1afa297938d40185957993d09c69b1946dfed00d
Sam <69326235+Sam959595@users.noreply.github.com>
2022-11-10 14:56:10 +0300

    Update book-read.js

--- js/book/book-read.js
+++ js/book/book-read.js
@@ -1,8 +1,8 @@
-window.addEventListener('touchstart', () => {
-    $('section.ww').click( () => {
+window.addEventListener('touchend', () => {
+    $('section.ww').click(() => {
         let vv = $('section.ee'),
-        nn = $('section.ww')
-    
+            nn = $('section.ww')
+
         vv[0].classList.add('eee')
         nn[0].classList.add('www')
     })
@@ -10,47 +10,51 @@ window.addEventListener('touchstart', () => {
 
 window.addEventListener('touchmove', () => {
     let vv = $('section.ee'),
-    nn = $('section.ww')
+        nn = $('section.ww')
 
     vv[0].classList.remove('eee')
     nn[0].classList.remove('www')
 })
 
+//
+
 window.addEventListener('DOMContentLoaded', () => {
     let vv = $('section.ee'),
-    nn = $('section.ww')
+        nn = $('section.ww')
 
-    nn.mouseenter( () => {
+    nn.mouseenter(() => {
         vv[0].classList.add('eee')
         nn[0].classList.add('www')
     })
-    vv.mouseleave( () => {
+    vv.mouseleave(() => {
         vv[0].classList.remove('eee')
         nn[0].classList.remove('www')
     })
 })
 
+//
+
 window.addEventListener('change', () => {
     let vv = $('#bb').val(),
-    nn = $('section.vv')
+        nn = $('section.vv')
 
     if (vv == '1') {
-        nn[0].classList.add('yyo')
-        nn[0].classList.remove('yyu', 'yyy')
+        nn[0].classList.add('y')
+        nn[0].classList.remove('yy', 'yyy')
     } else if (vv == '2') {
-        nn[0].classList.add('yyu')
-        nn[0].classList.remove('yyo', 'yyy')
+        nn[0].classList.add('yy')
+        nn[0].classList.remove('y', 'yyy')
     } else if (vv == '3') {
-        nn[0].classList.remove('yyu', 'yyo', 'yyy')
+        nn[0].classList.remove('yy', 'y', 'yyy')
     } else if (vv == '4') {
         nn[0].classList.add('yyy')
-        nn[0].classList.remove('yyu', 'yyo')
+        nn[0].classList.remove('yy', 'y')
     }
 })
 
 window.addEventListener('change', () => {
     let vv = $('#nn').val(),
-    nn = $('section.vv')
+        nn = $('section.vv')
 
     if (vv == '1') {
         nn[0].classList.add('zzz')
@@ -65,24 +69,30 @@ window.addEventListener('change', () => {
 
 window.addEventListener('change', () => {
     let vv = $('#ee').val(),
-    nn = $('body')[0]
+        nn = $('body')[0]
 
     if (vv == '1') {
         nn.classList.add('rrr')
-        nn.classList.remove('ttt')
+        nn.classList.remove('ttt', 'kkk')
 
         $('meta[media="(prefers-color-scheme: light)"]').attr('content', 'white')
         $('meta[media="(prefers-color-scheme: dark)"]').attr('content', 'black')
     } else if (vv == '2') {
         nn.classList.add('ttt')
-        nn.classList.remove('rrr')
+        nn.classList.remove('rrr', 'kkk')
 
         $('meta[media="(prefers-color-scheme: light)"]').attr('content', '#feddbc')
         $('meta[media="(prefers-color-scheme: dark)"]').attr('content', '#120b08')
     } else if (vv == '3') {
-        nn.classList.remove('rrr', 'ttt')
+        nn.classList.remove('rrr', 'ttt', 'kkk')
 
         $('meta[media="(prefers-color-scheme: light)"]').attr('content', '#f2f2f9')
+        $('meta[media="(prefers-color-scheme: dark)"]').attr('content', '#1a1a1c')
+    } else if (vv == '4') {
+        nn.classList.add('kkk')
+        nn.classList.remove('rrr', 'ttt')
+
+        $('meta[media="(prefers-color-scheme: light)"]').attr('content', '#51545c')
         $('meta[media="(prefers-color-scheme: dark)"]').attr('content', '#1a1a1c')
     }
 })
