01efad109424bc32defd0e78eb6eb15e2c8ba4ba
Sam <69326235+Sam959595@users.noreply.github.com>
2022-12-03 03:19:19 +0500

    Update book-buy2.js

--- js/book/book-buy2.js
+++ js/book/book-buy2.js
@@ -6,3 +6,15 @@ window.addEventListener('DOMContentLoaded', () => {
         (v[0].textContent === 'ПОДПИСАТЬСЯ') ? v[0].classList.toggle('aa') : v[0].classList.toggle('aa')
     })
 })
+
+window.addEventListener('DOMContentLoaded', () => {
+    let a = $('#uu'),
+    b = $('#aa')
+
+    b.click( () => {
+        a[0].classList.toggle('u')
+        b[0].classList.toggle('a')
+
+        console.log('ghb')
+    })
+})
