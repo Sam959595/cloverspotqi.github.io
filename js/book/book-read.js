fa11a885d4c84499b12721cf5d60fdde4a67e765
Sam <69326235+Sam959595@users.noreply.github.com>
2022-12-25 15:04:22 +0500

    Update book-read.js

--- js/book/book-read.js
+++ js/book/book-read.js
@@ -90,40 +90,40 @@ window.addEventListener('change', () => {
 
     if (vv == '1') {
         nn.classList.add('rrr')
-        mm.classList.add('rrrr')
+        mm.classList.add('rrr')
         nn.classList.remove('ttt', 'kkk', 'qqq')
-        mm.classList.remove('tttt', 'kkkk', 'qqqq')
+        mm.classList.remove('ttt', 'kkk', 'qqq')
 
         $('meta[media="(prefers-color-scheme: light)"]').attr('content', 'white')
         $('meta[media="(prefers-color-scheme: dark)"]').attr('content', 'black')
     } else if (vv == '2') {
         nn.classList.add('ttt')
-        mm.classList.add('tttt')
+        mm.classList.add('ttt')
         nn.classList.remove('rrr', 'kkk', 'qqq')
-        mm.classList.remove('rrrr', 'kkkk', 'qqqq')
+        mm.classList.remove('rrr', 'kkk', 'qqq')
 
         $('meta[media="(prefers-color-scheme: light)"]').attr('content', '#e0dacc')
         $('meta[media="(prefers-color-scheme: dark)"]').attr('content', '#26221d')
     } else if (vv == '3') {
         nn.classList.remove('rrr', 'ttt', 'kkk', 'qqq')
-        mm.classList.remove('rrrr', 'tttt', 'kkkk', 'qqqq')
+        mm.classList.remove('rrr', 'ttt', 'kkk', 'qqq')
 
         $('meta[media="(prefers-color-scheme: light)"]').attr('content', '#eeeded')
         $('meta[media="(prefers-color-scheme: dark)"]').attr('content', '#353539')
     } else if (vv == '4') {
         nn.classList.add('kkk')
-        mm.classList.add('kkkk')
+        mm.classList.add('kkk')
         nn.classList.remove('rrr', 'ttt', 'qqq')
-        mm.classList.remove('rrrr', 'tttt', 'qqqq')
+        mm.classList.remove('rrr', 'ttt', 'qqq')
 
         $('meta[media="(prefers-color-scheme: light)"]').attr('content', '#ddd')
         $('meta[media="(prefers-color-scheme: dark)"]').attr('content', '#1a1a1c')
     }
     else if (vv == '5') {
         nn.classList.add('qqq')
-        mm.classList.add('qqqq')
+        mm.classList.add('qqq')
         nn.classList.remove('rrr', 'ttt', 'kkk')
-        mm.classList.remove('rrrr', 'tttt', 'kkkk')
+        mm.classList.remove('rrr', 'ttt', 'kkk')
 
         $('meta[media="(prefers-color-scheme: light)"]').attr('content', '#f2f2f9')
         $('meta[media="(prefers-color-scheme: dark)"]').attr('content', '#51545c')
