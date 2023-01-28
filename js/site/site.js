38b6221850db78b80149f5b8e1bbc8d6abd01ade
Sam <69326235+Sam959595@users.noreply.github.com>
2023-01-28 06:00:10 +0500

    Delete site.js

--- js/site/site.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('DOMContentLoaded', () => {
-    let aa = window.innerWidth,
-    mm = document.createElement('style')
-
-    document.head.append(mm)
-    mm.innerHTML = ':root{--screen:' + aa + 'px}'
-})
