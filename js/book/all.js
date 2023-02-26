e73624d33562a8a837ee7c2d886d939dd5d52ade
Sam <69326235+Sam959595@users.noreply.github.com>
2023-02-27 04:28:58 +0500

    Update all.js

--- js/book/all.js
+++ js/book/all.js
@@ -13,9 +13,7 @@ $(document).ready(() => {
                 b.unobserve(x.target)
             }
         })
-    }, {
-        threshold: 0
-    });
+    })
 
     document.querySelectorAll('[data-url]').forEach(x => {
         ll.observe(x)
@@ -37,9 +35,7 @@ $(document).ready(() => {
                 b.unobserve(x.target)
             }
         })
-    }, {
-        threshold: 0
-    });
+    })
 
     document.querySelectorAll('[data-src]').forEach(x => {
         ll.observe(x)
@@ -74,14 +70,14 @@ window.addEventListener('DOMContentLoaded', () => {
 })
 
 // author, button follow
-window.addEventListener('click', () => {
-    let a = $('section.ww figcaption button')[0]
+// window.addEventListener('click', () => {
+//     let a = $('section.ww figcaption button')[0]
 
-    a.click(() => {
-        (a.textContent === 'В ЧИТАТЕЛИ') ? a.textContent = 'ЧИТАЮ' : a.textContent = 'В ЧИТАТЕЛИ';
-        (a.textContent === 'В ЧИТАТЕЛИ') ? a.classList.toggle('aa') : a.classList.toggle('aa')
-    })
-})
+//     a.click(() => {
+//         (a.textContent === 'В ЧИТАТЕЛИ') ? a.textContent = 'ЧИТАЮ' : a.textContent = 'В ЧИТАТЕЛИ';
+//         (a.textContent === 'В ЧИТАТЕЛИ') ? a.classList.toggle('aa') : a.classList.toggle('aa')
+//     })
+// })
 
 // book, button follow
 window.onload = () => {
@@ -98,32 +94,6 @@ window.onload = () => {
         }
     }
 }
-
-// double-tap post
-$(document).ready(() => {
-    $('section.dd li')[0].addEventListener('dblclick', () => {
-
-        if ($('section.dd li')[0].querySelector('icon') === null) {
-            //contain: paint $('section.dd li')[0].style.position = 'relative';
-
-            let vv = document.createElement('icon');
-            $('section.dd li')[0].appendChild(vv);
-
-            $('section.dd li icon')[0].style.cssText = 'opacity: 1; transition: opacity .2s ease-out';
-
-            setTimeout(() => {
-                $('section.dd li icon')[0].style.opacity = '0';
-
-                setTimeout(() => {
-                    $('section.dd li icon')[0].remove();
-                    $('section.dd li')[0].removeAttribute('style');
-                }, 200)
-            }, 900)
-        } else {
-            return false;
-        }
-    })
-})
 
 // library, buttom edit
 window.addEventListener('DOMContentLoaded', () => {
@@ -161,3 +131,59 @@ window.onload = () => {
         }
     }
 }
+
+// double-tap post
+$(document).ready(() => {
+    $('section.dd li')[0].addEventListener('dblclick', () => {
+
+        if ($('section.dd li')[0].querySelector('span') === null) {
+            //contain: paint $('section.dd li')[0].style.position = 'relative';
+
+            let vv = document.createElement('span');
+            $('section.dd li')[0].appendChild(vv);
+
+            $('section.dd li span')[0].style.cssText = 'opacity: 1; transition: opacity .2s ease-out';
+
+            setTimeout(() => {
+                $('section.dd li span')[0].style.opacity = '0';
+
+                setTimeout(() => {
+                    $('section.dd li span')[0].remove();
+                    $('section.dd li')[0].removeAttribute('style');
+                }, 200)
+            }, 900)
+        } else {
+            return false;
+        }
+    })
+})
+
+//
+
+$.ajax({
+    url: '/book/json-book.json',
+    method: 'get',
+    dataType: 'json',
+    data: {text: 'ghb'},
+    cache: false,
+    success: ggg = () => {
+        console.log(ggg);
+    }
+})
+
+// all, lazy load image url(a)
+$(document).ready(() => {
+    let vv = document.querySelector('video');
+    vv.playsInline = 'true';
+    vv.muted = 'true';
+    vv.loop = 'true';
+    vv.autoplay = 'false';
+    vv.play();
+
+    vv.addEventListener('touchstart', () => {
+        vv.pause();
+    })
+    vv.addEventListener('touchend', () => {
+        vv.play();
+    })
+})
