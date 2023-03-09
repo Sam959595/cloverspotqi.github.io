416254b3d2c78176e675fef5b0ec1a5d6188d051
Sam <69326235+Sam959595@users.noreply.github.com>
2023-03-09 05:24:54 +0500

    Update all.js

--- js/book/all.js
+++ js/book/all.js
@@ -1,3 +1,69 @@
+// document.addEventListener('DOMContentLoaded', () => {
+//     let vv = async () => {
+//         let t = await fetch('/book/json-book.json'),
+//         b = await t.json();
+
+//         b = b.splice(0, 8);
+
+//         for (a in b) {
+//             $('#kk')[0].innerHTML +=
+//             `<li>
+//             <div class="a">
+//                 <div class="b">
+//                     <div>
+//                         <h6>${b[a].gg}</h6>
+//                         <time datetime="${b[a].tt}">8 минут назад</time>
+//                     </div>
+//                     <a href="#" data-url="${b[a].ii}"></a>
+//                 </div>
+//                 <h5>${b[a].kk}</h5>
+//             </div>
+//             <div class="f" data-url="${b[a].jj}">
+//                 <div class="g">
+//                     <div class="m">
+//                         <div class="p">
+//                             <h6>Давно выяснено, что при оценке дизайна и композиции читаемый текст мешает сосредоточиться.</h6>
+//                             <h6>Давно выяснено, что при оценке дизайна и композиции читаемый текст мешает сосредоточиться.</h6>
+//                             <h6>Давно выяснено, что при оценке дизайна и композиции читаемый текст мешает сосредоточиться.</h6>
+//                         </div>
+//                         <a href="#">
+//                             <div data-url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1WV9cJQRlCCeqSvu1dNAZb6ls-qcqqmTiLQ&usqp=CAU"></div>
+//                             <div data-url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTP4bAILHs6UChbrMFjFX8WEV_wS5fO0QciA&usqp=CAU"></div>
+//                             <div data-url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1UZlSKB-d8Rme5zPjOe0kRSmEQ7U7jJwrbw&usqp=CAU"></div>
+//                             <div data-url="https://avatars.mds.yandex.net/i?id=53cb73b3116273dc384c00772c75c670-5220865-images-thumbs&n=13&exp=1"></div>
+//                         </a>
+//                     </div>
+//                     <p>Нравится Вам и ещё 2 058</p>
+//                 </div>
+//             </div>
+//             </li>`
+//         }
+//     }
+//     vv()
+// })
+
+// double-tap post
+$(document).ready(() => {
+    $('section.e li')[0].addEventListener('dblclick', () => {
+
+        if ($('section.e li')[0].querySelector('span') == null) {
+            let c = document.createElement('span');
+            
+            $('section.e li div:first-child')[0].appendChild(c);
+
+            setTimeout(() => {
+                $('section.e li span')[0].style.opacity = '0';
+
+                setTimeout(() => {
+                    $('section.e li span')[0].remove()
+                }, 200)
+            }, 900)
+        } else {
+            return false
+        }
+    })
+})
+
 // all, lazy load image url(a)
 $(document).ready(() => {
     let ll = new IntersectionObserver((a, b) => {
@@ -131,71 +197,45 @@ window.addEventListener('DOMContentLoaded', () => {
 //     }
 // }
 
-// double-tap post
-$(document).ready(() => {
-    $('section.dd li')[0].addEventListener('dblclick', () => {
-
-        if ($('section.dd li')[0].querySelector('span') === null) {
-            //contain: paint $('section.dd li')[0].style.position = 'relative';
-
-            let vv = document.createElement('span');
-            $('section.dd li')[0].appendChild(vv);
-
-            $('section.dd li span')[0].style.cssText = 'opacity: 1; transition: opacity .2s ease-out';
-
-            setTimeout(() => {
-                $('section.dd li span')[0].style.opacity = '0';
-
-                setTimeout(() => {
-                    $('section.dd li span')[0].remove();
-                    $('section.dd li')[0].removeAttribute('style');
-                }, 200)
-            }, 900)
-        } else {
-            return false;
-        }
-    })
-})
-
 // all, lazy load image url(a)
 $(document).ready(() => {
-    let pp = $('section.e .k .g')[0],
-    vv = document.querySelector('video');
+    let pp = $('section.e .v')[0],
+    v = document.querySelector('video');
 
     let ll = new IntersectionObserver(a => {
         a.forEach(x => {
             if (x.isIntersecting) {
                 x.target.play();
-                
-                pp.addEventListener('touchstart', ggg = () => {
-                    vv.pause()
+
+                pp.addEventListener('touchstart', sss = () => {
+                    v.pause()
                 })
-                pp.addEventListener('touchend', kkk = () => {
-                    vv.play()
+                pp.addEventListener('touchend', eee = () => {
+                    v.play()
                 })
             } else {
                 x.target.pause();
 
-                pp.removeEventListener('touchstart', ggg)
-                pp.removeEventListener('touchend', kkk)
+                pp.removeEventListener('touchstart', sss)
+                pp.removeEventListener('touchend', eee)
             }
         })
     }, {
-        threshold: .5
+        threshold: .6
     })
     document.querySelectorAll('video').forEach(x => {
         ll.observe(x)
     })
 
-    $('section.e svg')[0].addEventListener('click', () => {
+    $('section.e .t svg')[0].addEventListener('click', () => {
         if ($('video').prop('muted')) {
             $('video').prop('muted', false);
 
-            $('section.e svg path')[0].setAttribute('fill', '#ffffffcc')
+            $('section.e .t path')[0].setAttribute('fill', '#ffffffcc')
         } else {
             $('video').prop('muted', true);
 
-            $('section.e svg path')[0].setAttribute('fill', '#ffffff20')
+            $('section.e .t path')[0].setAttribute('fill', '#ffffff20')
         }
     })
 })
