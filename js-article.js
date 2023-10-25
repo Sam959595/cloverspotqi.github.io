a04e87769652bc8584a9005ef421660ab810a193
Sam <69326235+Sam959595@users.noreply.github.com>
2023-10-25 21:56:00 +0500

    Update js-article.js

--- js-article.js
+++ js-article.js
@@ -18,16 +18,19 @@ document.addEventListener('dblclick', () => {
     }
 })
 
+// comment
+$('section.a span:first-child').click(() => {
+    $('section.s')[0].style = 'pointer-events: auto; opacity: 1'
+})
+
 // bookmark counter
-$('section.a span+span')[0].addEventListener('click', () => {
+$('section.a span+span').click(() => {
     let a = $('section.a span+span h6')[0].innerText;
-    let b = Number(a) + 1;
-    $('section.a span+span h6')[0].innerText = b;
-    console.log(b);
-    $('section.a span+span h5')[0].classList.toggle('a');
+    $('section.a span+span h6')[0].innerText = Number(a) + 1;
+    $('section.a span+span h5')[0].classList.toggle('a')
 })
 
 // bookmark icon
-$('svg')[0].addEventListener('click', () => {
+$('svg').click(() => {
     $('svg path')[0].classList.toggle('b')
 })
