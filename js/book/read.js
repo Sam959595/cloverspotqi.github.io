57a4e40cb25d35a1fdc56486029754351f5dcf5a
Sam <69326235+Sam959595@users.noreply.github.com>
2023-11-01 21:26:50 +0500

    Update read.js

--- js/book/read.js
+++ js/book/read.js
@@ -1,9 +1,9 @@
 // tab nav
-$('section.s div:first-child')[0].addEventListener('dblclick', () => {
+$('section.p div:first-child')[0].addEventListener('dblclick', () => {
     $('section.n')[0].style = 'pointer-events: auto; opacity: 1'
 })
 
-$('section.s div:last-child')[0].addEventListener('dblclick', () => {
+$('section.p div:last-child')[0].addEventListener('dblclick', () => {
     $('section.e')[0].style = 'pointer-events: auto; opacity: 1';
 })
 
