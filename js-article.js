08e37ebe258e9f97f02231f157612b966abac855
Sam <69326235+Sam959595@users.noreply.github.com>
2024-11-24 16:58:16 +0500

    Update js-article.js

--- js-article.js
+++ js-article.js
@@ -21,11 +21,13 @@ $('article').dblclick(() => {
 
 // comment
 $('section.a button:last-child').click(() => {
-    $('body')[0].style = 'overflow: hidden; height: 100vh';
+    $('html')[0].style = 'box-sizing: border-box; overflow: hidden';
+    $('body')[0].style = 'box-sizing: border-box; overflow: hidden';
     $('section.s')[0].style = 'pointer-events: auto; opacity: 1'
 })
 
 $('section.s button').click(() => {
+    $('html')[0].removeAttribute('style');
     $('body')[0].removeAttribute('style');
     $('section.s')[0].removeAttribute('style')
 })
@@ -52,3 +54,74 @@ document.querySelectorAll('textarea').forEach(x => {
         }
     })
 })
+
+// img load
+document.querySelectorAll('img').forEach(x => {
+    new IntersectionObserver((a, b) => {
+        a.forEach(x => {
+            let a = x.target;
+            if (x.isIntersecting) {
+                b.unobserve(a);
+                let m = new Image();
+                m.src = a.dataset.i;
+                m.onload = () => {
+                    a.src = a.dataset.i;
+                    a.removeAttribute('data-i')
+                }
+            }
+        })
+    }).observe(x)
+})
+
+// video load
+document.querySelectorAll('video').forEach(x => {
+    new IntersectionObserver((a, b) => {
+        a.forEach(x => {
+            let a = x.target;
+            if (x.isIntersecting) {
+                b.unobserve(a);
+                a.src = a.dataset.v;
+                let t = setInterval(() => {
+                    if (a.currentTime > 0) {
+                        clearInterval(t);
+                        a.removeAttribute('data-v')
+                    }
+                }, 500)
+            }
+        })
+    }).observe(x)
+})
+
+// video observer play/pause
+document.querySelectorAll('video').forEach(x => {
+    new IntersectionObserver(x => {
+        x.forEach(x => {
+            if (x.isIntersecting) {
+                x.target.play();
+                $('section.h')[0].style.cssText = 'opacity: 1; pointer-events: initial'
+            } else {
+                x.target.pause();
+                $('section.h')[0].removeAttribute('style')
+            }
+        })
+    }, {
+        threshold: .2
+    }).observe(x)
+})
+
+// video toggle sound
+$('section.h')[0].addEventListener('click', () => {
+    if ($('video').prop('muted')) {
+        $('video').prop('muted', false);
+        $('section.h svg')[0].setAttribute('opacity', '.4')
+    } else {
+        $('video').prop('muted', true);
+        $('section.h svg')[0].setAttribute('opacity', '.1')
+    }
+})
+
+// video pause if exit tab browser
+window.onblur = () => {
+    $('video').prop('muted', true);
+    $('section.h svg')[0].setAttribute('opacity', '.1')
+}
