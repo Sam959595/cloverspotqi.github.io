1c7b3424a0c23d34fa37fef8a4b5dab549eced92
Sam <69326235+Sam959595@users.noreply.github.com>
2024-11-26 01:59:49 +0500

    Update js-article.js

--- js-article.js
+++ js-article.js
@@ -24,8 +24,8 @@ $('article').dblclick(() => {
 
 // comment
 $('section.a button:last-child').click(() => {
-    $('html')[0].style = 'box-sizing: border-box; overflow: hidden';
-    $('body')[0].style = 'box-sizing: border-box; overflow: hidden';
+    $('html')[0].style = 'overflow: hidden';
+    $('article, section.a')[0].style = 'touch-active: none';
     $('section.s')[0].style = 'pointer-events: auto; opacity: 1'
 })
 
